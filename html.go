964f550d8dac12190dc5da298722ab6b35d6e307
Michael Gasch <embano1@live.com>
2016-11-03 22:14:24 +0100

    Improvements

---
+++ html.go
@@ -0,0 +1,31 @@
+package main
+
+var html = `
+<div>
+<h1>Hi there!</h1>
+</div>
+<div>
+<p>Here I&acute;ll provide you with some stats about...me :)</p>
+<table style="height: 78px;" border="1" width="313">
+<tbody>
+<tr>
+<td><strong>Property</strong></td>
+<td><strong>Value</strong></td>
+</tr>
+<tr>
+<td>Version</td>
+<td>%s</td>
+</tr>
+<tr>
+<td>Hostname</td>
+<td>%s</td>
+</tr>
+<tr>
+<td>IP</td>
+<td>%s</td>
+</tr>
+</tbody>
+</table>
+<p>&nbsp;</p>
+</div>
+`
--- main.go
+++ main.go
@@ -3,28 +3,50 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"os/signal"
 )
 
-var html = `
-<html>
-<h1>Hi there!</h1>
-<p>My version is: %s</p>
-<p>My hostname is: %s</p>
-</html>
-`
-
+// HTML response
 func handler(w http.ResponseWriter, r *http.Request) {
 	hostname, err := os.Hostname()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Fprintf(w, html, version, hostname)
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/* for i, addr := range addrs {
+	    fmt.Printf("%d %v\n", i, addr)
+	}*/
+
+	log.Printf("%s %s - %s", r.Method, r.URL.Path, r.UserAgent())
+	fmt.Fprintf(w, html, version, hostname, addrs[1])
+}
+
+func cleanup() {
+	log.Println("Got CTRL-C...cleaning up now.")
 }
 
 func main() {
+	log.Println("Starting up...")
+	log.Printf("You´re running version %s of this program.", version)
+
+	// Catch CRTL-C
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		cleanup()
+		os.Exit(0)
+	}()
+
+	// Call handler func (create html) and start serving
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
